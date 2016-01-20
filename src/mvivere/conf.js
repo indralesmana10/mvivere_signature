c3c963dfd17eb2ea5479b00118f0238d840a4851
indralesmana10 <indralesmana.mmb@gmail.com>
2016-01-20 17:49:37 +0700

    update

--- src/mvivere/conf.js
+++ src/mvivere/conf.js
@@ -7,8 +7,8 @@ module.exports = [
     contactMain: 'Call <a href="tel:004581100200"><span>(45) 81100200</span></a> or email us at', // Wrap phone numbers like this to prevent iOS mail from making them blue.
     contactMail: 'info@tryplay.dk',
     slogan: 'LED Pylon. LED Wall. Digital Signage.',
-    logoUrl: 'https://informationscreen.com/manage/assets/images/play-logo.png',
-    logoAlt: 'Play. logo',
-    website: 'http://tryplay.dk'
+    logoUrl: 'http://i63.tinypic.com/2mot6yg.jpg',
+    logoAlt: 'mvivere logo',
+    website: '#'
   }
 ]
---
+++ src/signature/conf.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 'play',
+    signature: 'Best regards,',
+    name: 'VIVEK ASWANI',
+    position: 'Director',
+    contactMain: 'Call <a href="tel:004581100200"><span>(45) 81100200</span></a> or email us at', // Wrap phone numbers like this to prevent iOS mail from making them blue.
+    contactMail: 'info@tryplay.dk',
+    slogan: 'LED Pylon. LED Wall. Digital Signage.',
+    logoUrl: 'http://i63.tinypic.com/2mot6yg.jpg',
+    logoAlt: 'mvivere logo',
+    website: '#'
+  }
+]
